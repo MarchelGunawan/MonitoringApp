a698613a6d6d3311da57f5f8b2ef553dc355a4a3
Marchel Gunawan Dwi Steya <90387165+MarchelGunawan@users.noreply.github.com>
2025-02-23 15:48:58 +0700

    update Readme

--- obj/Debug/net8.0/MonitoringApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonitoringApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitoringApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda0a7f335a582ec60c80b773cdff384447aba1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9197cff9d8141377f4f64bc63fe0a643c14de42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitoringApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitoringApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
